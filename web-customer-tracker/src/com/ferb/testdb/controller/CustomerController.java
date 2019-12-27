fad6c2486d895d6d99c2f0a05266e1a8ec50589d
ferbjava <tooper@wp.pl>
2019-12-27 12:54:56 +0100

    section 30 completed

--- web-customer-tracker/src/com/ferb/testdb/controller/CustomerController.java
+++ web-customer-tracker/src/com/ferb/testdb/controller/CustomerController.java
@@ -5,22 +5,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.ferb.testdb.dao.CustomerDAO;
 import com.ferb.testdb.entity.Customer;
+import com.ferb.testdb.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
 	@Autowired
-	private CustomerDAO customerDao;
+	private CustomerService customerService;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomer(Model model) {
 		
-		List<Customer> customers = customerDao.getCustomers();
+		List<Customer> customers = customerService.getCustomers();
 		
 		model.addAttribute("customers", customers);
 		
---
+++ web-customer-tracker/src/com/ferb/testdb/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.ferb.testdb.service;
+
+import java.util.List;
+
+import com.ferb.testdb.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+}
