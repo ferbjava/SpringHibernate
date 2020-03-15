b652eb94ff350745c7e5e10c63ebc5557fa31ff3
ferbjava <tooper@wp.pl>
2020-03-15 21:11:40 +0100

    section 52 completed

---
+++ spring-security-demo-05-roles/src/main/java/com/ferb/spring_security_demo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.ferb.spring_security_demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+	@GetMapping("/")
+	public String showLanding() {
+		return "landing";
+	}
+
+	@GetMapping("/employees")
+	public String showHome() {
+		return "home";
+	}
+	
+}
