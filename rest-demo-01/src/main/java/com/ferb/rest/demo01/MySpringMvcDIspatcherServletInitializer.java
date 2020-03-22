ca55dbe035ef28daba8f3cc469026c2a01a6ddca
ferbjava <tooper@wp.pl>
2020-03-22 14:17:43 +0100

    section 58 completed

---
+++ rest-demo-01/src/main/java/com/ferb/rest/demo01/MySpringMvcDIspatcherServletInitializer.java
@@ -0,0 +1,23 @@
+package com.ferb.rest.demo01;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDIspatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {DemoAppConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
