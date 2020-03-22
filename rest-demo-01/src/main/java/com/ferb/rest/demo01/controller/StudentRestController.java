1b17e52873dff63d6a7f53f029ccd9e160041442
ferbjava <tooper@wp.pl>
2020-03-22 19:15:55 +0100

    section 60 completed

--- rest-demo-01/src/main/java/com/ferb/rest/demo01/controller/StudentRestController.java
+++ rest-demo-01/src/main/java/com/ferb/rest/demo01/controller/StudentRestController.java
@@ -3,7 +3,10 @@ package com.ferb.rest.demo01.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +15,25 @@ import com.ferb.rest.demo01.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+	
+	private List<Student> students;
+	
+	@PostConstruct
+	public void loadData() {
+		students = new ArrayList<>();
+		students.add(new Student("Jan", "Kowalski"));
+		students.add(new Student("Adam", "Nowak"));
+		students.add(new Student("Piotr", "Wiśniewski"));
+		
+	}
 
 	@GetMapping("/students")
 	public List<Student> getStudents() {
-		List<Student> students = new ArrayList<>();
-		students.add(new Student("Jan", "Kowalski"));
-		students.add(new Student("Adam", "Nowak"));
-		students.add(new Student("Piotr", "Wiśniewskie"));
-		
 		return students;
+	}
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		return students.get(studentId);
 	}
 }
