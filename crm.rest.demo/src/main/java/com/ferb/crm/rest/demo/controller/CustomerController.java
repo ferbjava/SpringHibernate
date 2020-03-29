8c1edf4a0581e972b4591114ad6408e77f587c84
ferbjava <tooper@wp.pl>
2020-03-29 15:39:31 +0200

    section 67 completed

--- crm.rest.demo/src/main/java/com/ferb/crm/rest/demo/controller/CustomerController.java
+++ crm.rest.demo/src/main/java/com/ferb/crm/rest/demo/controller/CustomerController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,4 +36,16 @@ public class CustomerController {
 		return customer;
 	}
 	
+	@PostMapping("/customer")
+	public Customer saveCustomer(@RequestBody Customer customer) {
+		customer.setId(0);
+		customerService.saveCustomer(customer);
+		return customer;
+	}
+	
+	@PutMapping("/customer")
+	public Customer updateCustomer(@RequestBody Customer customer) {
+		customerService.saveCustomer(customer);
+		return customer;
+	}
 }
